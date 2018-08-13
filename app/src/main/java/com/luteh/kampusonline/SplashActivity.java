5ea501eae91980298689b32cb218318803b38a3a
luteh <luthfanmaftuh@gmail.com>
2018-08-13 13:48:16 +0700

    Added Splash screen

---
+++ app/src/main/java/com/luteh/kampusonline/SplashActivity.java
@@ -0,0 +1,104 @@
+package com.luteh.kampusonline;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.luteh.kampusonline.common.util.PermissionHelper;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private PermissionHelper permissionHelper;
+
+    private Handler splashScreenHandler = null;
+    private Runnable splashScreenRunnable = null;
+    private final int SPLASH_TIME_MILLISECOND = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        initPermission();
+        //initSplash();
+    }
+
+    public void initPermission() {
+        permissionHelper = new PermissionHelper.Builder(this)
+                .withPermissions(
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.GET_ACCOUNTS)
+                .withListener(new PermissionHelper.OnPermissionCheckedListener() {
+                    @Override
+                    public void onPermissionGranted(boolean isPermissionAlreadyGranted) {
+                        //splashPresenter.initSplash();
+                        initSplash();
+                    }
+
+                    @Override
+                    public void onPermissionDenied() {
+                        showPermissionDialog();
+                    }
+                })
+                .build();
+
+        permissionHelper.requestPermission();
+    }
+
+    public void initSplash() {
+
+        if (splashScreenRunnable == null) {
+            splashScreenRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    //splashView.openHome();
+                    startActivityWithFade(LoginActivity.class);
+                }
+            };
+        }
+
+        if (splashScreenHandler == null) {
+            splashScreenHandler = new Handler();
+        }
+
+        splashScreenHandler.postDelayed(splashScreenRunnable, SPLASH_TIME_MILLISECOND);
+    }
+
+    private void showPermissionDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AppTheme);
+
+        builder.setTitle(getResources().getString(R.string.label_warning));
+        builder.setCancelable(false);
+        builder.setMessage(getResources().getString(R.string.label_message_apps_requirement));
+        builder.setPositiveButton(getResources().getString(R.string.label_show_permission), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                initPermission();
+            }
+        });
+        builder.setNegativeButton(getResources().getString(R.string.label_close_app), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                finish();
+            }
+        });
+        builder.show();
+    }
+
+    public final void startActivityWithFade(Class clazz) {
+        Intent intent = new Intent(this, clazz);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+}
