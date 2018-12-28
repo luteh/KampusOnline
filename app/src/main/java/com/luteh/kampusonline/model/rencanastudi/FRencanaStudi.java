6c507e500226441af72256673fb597c1fa2633cf
luteh <luthfanmaftuh@gmail.com>
2018-12-28 15:23:11 +0700

    Add rencana studi model

---
+++ app/src/main/java/com/luteh/kampusonline/model/rencanastudi/FRencanaStudi.java
@@ -0,0 +1,20 @@
+package com.luteh.kampusonline.model.rencanastudi;
+
+import java.util.List;
+
+/**
+ * Created by Luthfan Maftuh on 28/12/2018.
+ * Email luthfanmaftuh@gmail.com
+ */
+public class FRencanaStudi {
+    public List<Semester> semester_1 = null;
+    public List<Semester> semester_2 = null;
+    public List<Semester> semester_3 = null;
+    public List<Semester> semester_4 = null;
+    public List<Semester> semester_5 = null;
+    public List<Semester> semester_6 = null;
+    public List<Semester> semester_7 = null;
+    public List<Semester> semester_8 = null;
+    public Integer jatah_sks;
+    public String tahun_akademik;
+}
---
+++ app/src/main/java/com/luteh/kampusonline/model/rencanastudi/Semester.java
@@ -0,0 +1,13 @@
+package com.luteh.kampusonline.model.rencanastudi;
+
+/**
+ * Created by Luthfan Maftuh on 28/12/2018.
+ * Email luthfanmaftuh@gmail.com
+ */
+public class Semester {
+    public Integer no;
+    public String kode;
+    public String mata_kuliah;
+    public String nilai;
+    public String sks;
+}
